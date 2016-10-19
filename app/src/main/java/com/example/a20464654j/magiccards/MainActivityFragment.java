5a9f53552c8f1e2fb156d4c56f9f358cd4a96c45
Fabian <20464654j@iespoblenou.org>
2016-10-19 13:39:26 +0200

    Afegit menu al fragment, notificat a l'Activity que hem afegit el menu. Afegits alguns comentaris

--- app/src/main/java/com/example/a20464654j/magiccards/MainActivityFragment.java
+++ app/src/main/java/com/example/a20464654j/magiccards/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.a20464654j.magiccards;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,39 +10,54 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    private ArrayList<String> items;
+    // ArrayList amb les cartes
+    private ArrayList<String> alCartes;
+
     private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Opcio per a mostrar el menu ( en aquest cas del MainActivityFragment)
+        setHasOptionsMenu(true);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvCartes = (ListView) view.findViewById(R.id.lvCards);
 
-       // String[] info = {"text1", "text2", "text3", "text4", "text5", "text6", "text7"};
-
-        items = new ArrayList<>();
+        // ArrayList temporal per a provar el ListView del fragment_main
+        alCartes = new ArrayList<>();
         for( int i = 0; i < 1000; i++){
-            items.add("pene " + i);
+            alCartes.add("elemento #" + i);
         }
+
+        // Adaptador per a incloure cada carta de l'ArrayList de les cartes al TextView del lv_cartes_linea
+        // que anira dintre de cada posicio del ListView del fragment_main
         adapter = new ArrayAdapter<>(
                 getContext(),
-                R.layout.lv_cards_row,
+                R.layout.lv_cartes_linea,
                 R.id.tvLinea,
-                items
+                alCartes
         );
 
+        // Per a cada carta del ListView inclou un adaptador
         lvCartes.setAdapter( adapter );
 
         return view;
