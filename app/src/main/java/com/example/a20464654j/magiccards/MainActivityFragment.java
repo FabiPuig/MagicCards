3f27c7f25d688f67db5b955c1c90bd563d65bf44
Fabian <20464654j@iespoblenou.org>
2016-10-19 20:20:26 +0200

    Actualitzades les opcions del menu. Quan es crea i quant seleccionem un objecte del menu

--- app/src/main/java/com/example/a20464654j/magiccards/MainActivityFragment.java
+++ app/src/main/java/com/example/a20464654j/magiccards/MainActivityFragment.java
@@ -4,6 +4,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -33,6 +36,7 @@ public class MainActivityFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,6 +44,7 @@ public class MainActivityFragment extends Fragment {
         
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        //Creem un ListView que anira vinculat al list view del fragment_main
         ListView lvCartes = (ListView) view.findViewById(R.id.lvCards);
 
         // ArrayList temporal per a provar el ListView del fragment_main
@@ -48,18 +53,43 @@ public class MainActivityFragment extends Fragment {
             alCartes.add("elemento #" + i);
         }
 
-        // Adaptador per a incloure cada carta de l'ArrayList de les cartes al TextView del lv_cartes_linea
-        // que anira dintre de cada posicio del ListView del fragment_main
+        // Adaptador per a incloure cada carta de l'ArrayList de les cartes al TextView del
+        // lv_cartes_linea que anira dintre de cada posicio del ListView del fragment_main
         adapter = new ArrayAdapter<>(
-                getContext(),
-                R.layout.lv_cartes_linea,
-                R.id.tvLinea,
-                alCartes
+                getContext(),               //en aquest fragment
+                R.layout.lv_cartes_linea,   //layout on posara el TextView
+                R.id.tvLinea,               //TextView text view en el que metera el contenido
+                alCartes                    //contenido del ListView
         );
 
-        // Per a cada carta del ListView inclou un adaptador
+        // Enllaça el contingut de l'adaptador al ListView ( en aquest cas al ListView fragment_main)
         lvCartes.setAdapter( adapter );
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate( R.menu.menu_cartes_fragment, menu);
+    }
+
+    // quan seleccionem un objecte del menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if( id == R.id.action_refresh){
+            refresh();
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void refresh(){
+
     }
 }
